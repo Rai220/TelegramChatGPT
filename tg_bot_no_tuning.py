724a7e1233ddc1b1bb7a9447168dc0623fb91b3e
Konstantin Krestnikov <rai220@gmail.com>
2023-03-17 09:55:53 +0300

    .

--- tg_bot_no_tuning.py
+++ tg_bot_no_tuning.py
@@ -3,6 +3,12 @@ import openai
 import time
 import tiktoken
 import os
+import json
+import flask
+import threading
+
+# Proxy server for accessing OpenAI API
+app = flask.Flask(__name__)
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
@@ -14,14 +20,24 @@ if not TG_TOKEN:
     print("Please set TG_TOKEN environment variable")
     exit()
 
+token = os.environ.get("TOKEN", None)
+if not token:
+    raise ValueError("TOKEN must be set")
+
+port = os.environ.get("PORT", 8080)
+
 tokenizer = tiktoken.get_encoding("cl100k_base")
-max_history = 3500 # History will be truncated after this length
+max_history = 7500 # History will be truncated after this length
 
 bot = telebot.TeleBot(TG_TOKEN)
 openai.api_key = OPENAI_API_KEY
-model = "gpt-3.5-turbo-0301"
+model = "gpt-4-0314"
 
+# Load history from file
 users = {}
+if os.path.exists("users.json"):
+    with open("users.json", "r") as f:
+        users = json.load(f)
 
 def log(text):
     # Print to screen and log file
@@ -47,30 +63,34 @@ def _get_user(id):
 
 
 def _process_rq(user_id, rq):
-    user = _get_user(user_id)
-    # if last prompt time > 60 minutes ago - drop context
-    if time.time() - user['last_prompt_time'] > 60*60:
-        last_text = ''
-        user['last_prompt_time'] = 0
-        user['history'] = _get_clear_history()
+    try:
+        user = _get_user(user_id)
+        # if last prompt time > 60 minutes ago - drop context
+        # if time.time() - user['last_prompt_time'] > 60*60*24:
+        #     user['last_prompt_time'] = 0
+        #     user['history'] = _get_clear_history()
 
-    if rq and len(rq) > 0 and len(rq) < 250:
-        log(f">>> ({user_id}) {rq}")
-        user['history'].append({"role": "user", "content": rq})
-        while(_count_tokens(user) > max_history):
-            user['history'].pop(0)
+        if rq and len(rq) > 0 and len(rq) < 250:
+            log(f">>> ({user_id}) {rq}")
+            user['history'].append({"role": "user", "content": rq})
+            # Truncate history but save first prompt
+            while(_count_tokens(user) > max_history):
+                user['history'].pop(1)
 
-        completion = openai.ChatCompletion.create(
-            model=model, messages=user['history'], temperature=0.7)
-        ans = completion['choices'][0]['message']['content']
-        log(f"<<< ({user_id}) {ans}")
-        user['history'].append({"role": "user", "content": ans})
-        user['last_prompt_time'] = time.time()
-        return ans
-    else:
-        user['last_prompt_time'] = 0
-        user['last_text'] = ''
-        return "!!! Error! Please use simple short texts"
+            completion = openai.ChatCompletion.create(
+                model=model, messages=user['history'], temperature=0.7)
+            ans = completion['choices'][0]['message']['content']
+            log(f"<<< ({user_id}) {ans}")
+            user['history'].append({"role": "user", "content": ans})
+            user['last_prompt_time'] = time.time()
+            return ans
+        else:
+            user['last_prompt_time'] = 0
+            user['last_text'] = ''
+            return "!!! Error! Please use simple short texts"
+    except Exception as e:
+        log(f"!!! Error: {e}")
+        return "Error! Please try again later"
 
 
 @bot.message_handler(commands=['start', 'help', 'clear'])
@@ -82,11 +102,90 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def process_message(message):
-    user_id = message.from_user.id
-    rq = message.text
-    ans = _process_rq(user_id, rq)
-    bot.send_message(message.chat.id, ans)
+    try:
+        user_id = message.from_user.id
+        rq = message.text
+        ans = _process_rq(user_id, rq)
+        bot.send_message(message.chat.id, ans)
+        # Save users
+        with open("users.json", "w") as f:
+            json.dump(users, f)
+    except Exception as e:
+        log(f"!!! Error: {e}")
+
+@app.route("/<path:path>", methods=["GET", "POST"])
+def proxy(path):
+    # Check token
+    if flask.request.args.get("token", None) != token:
+        return flask.jsonify({"error": "Invalid token"}), 403
+    
+    if flask.request.method == "GET":
+        response = openai.Requestor.request("get", path, params=flask.request.args)
+    elif flask.request.method == "POST":
+        response = openai.Requestor.request("post", path, params=flask.request.form)
+    return flask.jsonify(response)
 
 
-if __name__ == '__main__':
-    bot.polling()
+@app.route("/completion", methods=["POST", "GET"])
+def completion():
+    # Check token
+    try:
+        # Check token in header
+        if flask.request.headers.get("token", None) != token:
+            return flask.jsonify({"error": "Invalid token"}), 403
+        
+        data = flask.request.get_json()
+        if not data:
+            return flask.jsonify({"error": "No json data"}), 400
+
+        prompt = data.get("prompt", None)
+        if not prompt:
+            return flask.jsonify({"error": "Prompt must be set"}), 400
+        
+        stop = data.get("stop", ['###'])
+        engine = data.get("engine", "text-davinci-003")
+        max_tokens = data.get("max_tokens", 150)
+        temperature = data.get("temperature", 0.9)
+        
+        response = openai.Completion.create(
+            engine=engine,
+            prompt=prompt,
+            max_tokens=max_tokens,
+            temperature=temperature,
+            top_p=1,
+            best_of=1,
+            stop=stop
+        )
+        return flask.jsonify(response)
+    except Exception as e:
+        return flask.jsonify({"error": str(e)}), 500
+    
+
+@app.route("/chatcompletion", methods=["POST", "GET"])
+def chatcompletion():
+    # Check token
+    try:
+        if flask.request.args.get("token", None) != token:
+            return flask.jsonify({"error": "Invalid token"}), 403
+        
+        # Get json from body
+        data = flask.request.get_json()
+        if not data:
+            return flask.jsonify({"error": "No json data"}), 400
+        
+        model = data.get("model", "gpt-3.5-turbo-0301")
+        max_tokens = data.get("max_tokens", 150)
+        temperature = data.get("temperature", 0.9)
+        messages = data.get("messages", None)
+        
+        completion = openai.ChatCompletion.create(
+            model=model, messages=messages, temperature=temperature, max_tokens=max_tokens)
+        return flask.jsonify(completion)
+    except Exception as e:
+        return flask.jsonify({"error": str(e)}), 500
+    
+# Start server
+if __name__ == "__main__":
+    # Run bot polling in thread
+    bot_thread = threading.Thread(target=bot.polling)
+    app.run(host="0.0.0.0", port=port)
